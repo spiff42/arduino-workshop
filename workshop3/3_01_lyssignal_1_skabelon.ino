7468ab1d8e58c270854317e00fbcdb6f3151d766
Mikkel Holm Olsen <spiff@symlink.dk>
2022-10-30 18:07:13 +0100

    Added slides and code for workshop 3

---
+++ workshop3/3_01_lyssignal_1_skabelon.ino
@@ -0,0 +1,34 @@
+/**********************************************************************
+ * Lyssignal skabelon
+ * https://github.com/spiff42/arduino-workshop
+ * Coding Pirates Workshop, Lyngby - Microsoft, Oktober/November 2022
+ * Mikkel Holm Olsen
+ **********************************************************************/
+const byte traffic_lightA_red_pin = 2;
+const byte traffic_lightA_yellow_pin = 3;
+const byte traffic_lightA_green_pin = 4;
+
+void setup() {
+  pinMode(traffic_lightA_red_pin, OUTPUT);
+  // Tilføj kode der initialiserer de to andre pins
+}
+
+void loop() {
+  // 1. tilstand: rødt lys  
+  digitalWrite(traffic_lightA_red_pin, HIGH); // Tænd rødt
+  delay(7000);
+
+  // 2. tilstand: rødt + gult lys
+  //digitalWrite(...); // Tænd gult
+  delay(1000);
+
+  // 3. tilstand: grønt lys
+  // Sluk rødt
+  // Sluk gult
+  // Tænd grønt
+  delay(5000);
+
+  // 4. tilstand: gult lys
+  // ...
+  delay(1000);
+}
